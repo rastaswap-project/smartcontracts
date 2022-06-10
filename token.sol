220233389fe30ee88a9de5b9ba8bc0221f0a6972
rastaswap-project <105817595+rastaswap-project@users.noreply.github.com>
2022-06-10 17:28:02 -0300

    Update token.sol

--- token.sol
+++ token.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.11;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
-contract RastacoinToken is ERC20, ERC20Burnable {
+contract Token is ERC20, ERC20Burnable {
     constructor() ERC20("RastaCoin", "RCOIN") {
         // Generate Maximum Total Supply of RastaCoin
         _mint(msg.sender, 40000000 * 10 ** 18);
